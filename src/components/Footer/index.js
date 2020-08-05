3668896baa860327950121b9046e25ea28f7e2d3
Edson Jr <juninhogpe@gmail.com>
2020-08-05 16:58:38 -0300

    Primeiro Commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/logo.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="/">
+        <img className="Logo" src={Logo} alt="WorkarroundFlix" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Criado na
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
+        </a> por <a href="mailto:juninhogpe@gmail.com">
+          Edson Jr.
         </a>
       </p>
     </FooterBase>
