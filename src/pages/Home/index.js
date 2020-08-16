bcaf6338699e9ad30f01d5d5baf649ddca9bbff9
Edson Jr <juninhogpe@gmail.com>
2020-08-16 14:28:39 -0300

    Carrosel ajustado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import Menu from '../../components/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
+import Slider from '../../components/Slider';
 import Footer from '../../components/Footer';
 
 function Home() {
