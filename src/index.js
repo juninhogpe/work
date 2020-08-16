bcf643025cba01d2f34c1c62812af3d44cace6cc
Edson Jr <juninhogpe@gmail.com>
2020-08-16 13:03:47 -0300

    Pages Default, Alterações CSS, Footer e Menu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+const PaginaErro = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter>
+    <React.StrictMode>
+      <Switch>        
+        <Route path="/" component={Home} exact />        
+        <Route path="/cadastro/video" component={CadastroVideo} />
+        <Route path="/cadastro/categoria" component={CadastroCategoria} />
+        <Route path="/" component={PaginaErro} />
+      </Switch>
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
